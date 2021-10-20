2cc554be2fcbce7e450aa1e257561dc3ccb1beff
unknown <SENA@CALAPRCEAFSD5PJ.FormacionSena.RED>
2021-10-20 18:07:20 -0500

    Pendiente por ingresar los datos de las cartas de cada jugador

--- Modelos/PartidaJugadorCartas.php
+++ Modelos/PartidaJugadorCartas.php
@@ -8,26 +8,24 @@ class PartidasJugadorCartasModelo{
 
     }
 
-    public function consultarCartasPartida(){
+    public function consultarCartasPartida($codigo){
         global $DB;
-
         $query = "SELECT * FROM rel_partida_jugador_cartas WHERE id_partida =  (SELECT id_partida FROM partidas WHERE codigo LIKE ?) ";
-        $res = $DB->query($query, array( $codigo )); 
+        $res = $DB->query($query, array($codigo)); 
         return $res;
     }
 
     public function repartirCartasJugador($codigo, $idjugador, $cartastotales){
         global $DB;
-
         $query = "INSERT INTO rel_partida_jugador_cartas (id_partida, id_jugador, idcarta1, idcarta2, idcarta3, idcarta4, fecha, orden_llegada) 
                 VALUES ((SELECT id_partida FROM partidas WHERE codigo LIKE ?) ,?,?,?,?,?,?,?)";
         $rel = $DB->query($query, array(
             $codigo,
             $idjugador,
-            $carta1,
-            $carta2,
-            $carta3,
-            $carta4,
+            1,
+            2,
+            3,
+            4,
             date("Y-m-d H:m:i"),
             1
         ));
