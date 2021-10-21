49576067066e4bc90ebb419842e5ccc858d26cfb
Angelica Giraldo <angelicagiraldo2@gmail.com>
2021-10-21 13:10:12 -0500

    Modelo y controlador de PartidaJugadorTablas y su insert

--- api/index.php
+++ api/index.php
@@ -7,14 +7,21 @@ require_once("../lib/config.php");
 
 require_once("../Modelos/Jugadores.php");
 require_once("../Controladores/JugadoresControlador.php");
+
 require_once("../Modelos/Partidas.php");
 require_once("../Controladores/PartidasControlador.php");
+
 require_once("../Modelos/Cartas.php");
 require_once("../Controladores/CartasControlador.php");
+
 require_once("../Modelos/PartidaSecreto.php");
 require_once("../Controladores/PartidaSecretoControlador.php");
+
 require_once("../Modelos/PartidaJugadorCartas.php");
 require_once("../Controladores/PartidaJugadorCartasControlador.php");
+
+require_once("../Modelos/PartidaJugadorTabla.php");
+require_once("../Controladores/PartidaJugadorTablaControlador.php");
 
 // Pintamos el archivo como salida JSON para consultas desde Javascript
 header('Content-Type: application/json');
@@ -85,7 +92,7 @@ if(isset($_POST["processing"])):
                     );
                 break;
                 default:
-                    //unset($_SESSION["codigo"]); /* Quitamos la sesión porque no tiene partida asignada */
+                    unset($_SESSION["codigo"]); /* Quitamos la sesión porque no tiene partida asignada */
                     $salida = array(
                         "codigo" => 999, /**/
                         "mensaje" => "Código temporal de pruebas. Es que todo va ok.",
